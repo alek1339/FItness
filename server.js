d4bacebba6ab4c03ce2ad810cbff44a9678f54d3
alek1339 <aleksandermihaylov@gmail.com>
2020-07-02 13:24:39 +0300

    update dev

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+const users = require('./routes/users')
+
 const app = express()
 
 // Bodyparser Middleware
@@ -17,5 +19,10 @@ mongoose
     .then(() => console.log('Mongodb Connected'))
     .catch(err => console.log(err))
 
+// Use Routes
+app.use('/users', users)
+
 
 const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
